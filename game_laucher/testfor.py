f0685669ac175c61e3fd33820b84ee7ca2132982
lightboxofficial <lightboxofficial@163.com>
2025-07-09 08:58:23 +0000

    Created a graphical interface test for the launcher and tested the graphical interface based on the customtkinter library

---
+++ game_laucher/testfor.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+from customtkinter import*
+from tkinter import messagebox
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+root = ctk.CTk()
+root.geometry("1600x900")
+root.title("Test for Game Launcher")
+
+root.mainloop()
