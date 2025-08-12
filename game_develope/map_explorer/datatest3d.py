b2d13a27b16ccdd4decc6f5290bfc3356d5ca14b
lightboxofficial <lightboxofficial@163.com>
2025-08-12 10:05:05 +0000

    I have created test3D as a placement test for ursina, but I haven't figured out the absolute path yet, so I'm not really a steamed bun

--- game_develope/map_explorer/datatest3d.py
+++ game_develope/map_explorer/datatest3d.py
@@ -1,5 +1,25 @@
-try:
-    import ursina
-    print("ursina 导入成功！")
-except ImportError:
-    print("未安装 ursina")
+from ursina import *
+
+app = Ursina()
+
+texture_map = {
+    0: r'C:\Users\i\Pictures\texture0.png',
+    1: r'C:\Users\i\Pictures\texture1.jpg',
+    2: r'C:\Users\i\Pictures\texture2.png'
+}
+#创建字典，这里是事例，因为我还没有搞清楚codespace的绝对路径是什么样的
+#所以我真的是个蒸馍，我也不知道咋弄，所以就先这样吧......
+
+def setblock(x, y, z, texture_index):
+    texture_path = texture_map.get(texture_index, None)
+    if texture_path:
+        texture_obj = load_texture(texture_path)
+    else:
+        texture_obj = None  #啥都没有，就加载小白条纹理
+    cube = Entity(
+        model='cube',
+        texture=texture_obj,
+        scale=(1, 1, 1),
+        position=(x, y, z)
+    )
+app.run() 
